aa953470ff525ed6e16d0feaa702a62c1b194523
Shebraz Hashmi <sherbaz.hashmi@gmail.coim>
2019-11-03 12:23:32 +1100

    Somewhat working grid for selection of community

--- src/landing/landing.js
+++ src/landing/landing.js
@@ -3,16 +3,21 @@ import { inject } from 'aurelia-framework';
 
 @inject(GISService)
 export class Landing {
+  selectedCommunity = null;
   constructor(GISService) {
     this.gisService = GISService;
     this.message = "123"
+    this.communities = [];
     console.log(this.message)
   }
 
-  async attached() {
-    const communities = await this.gisService.getData("community");
-    this.message = communities.Community[0].name
-    console.log(communities)
+  attached() {
+    this.gisService.getData("community").then((communities) => {
+      this.communities = communities.Community;
+      console.log(communities.Community)
+    }, reason => {
+      console.log(reason);
+    });
   }
 
   

4d16d4c41c8662d746360a11d61d78b32c490c88
steph <stephen.ma@zepben.com>
2019-11-03 19:36:20 +1100

    changed on edible

--- src/plant/edible-plants.js
+++ src/plant/edible-plants.js
@@ -1,6 +1,88 @@
+import $ from 'jquery';
+import {Router} from 'aurelia-router';
+
+
 export class EdiblePlants {
   constructor() {
     this.message = 'Hello world';
+  }
+
+  
+attached() {
+  let scriptURL = 'https://js.arcgis.com/4.13/';
+
+  let scriptElement = document.createElement('script');
+
+  scriptElement.src = scriptURL;
+
+  this.scriptElementInHead = document.querySelector('head').appendChild(scriptElement);
+
+  scriptElement.onload = () => {
+    console.log("LOADED")
+    $('head').append(` 
+    <meta charset="utf-8" />
+  <meta
+    name="viewport"
+    content="initial-scale=1,maximum-scale=1,user-scalable=no"
+  />
+      <style>
+        html,
+        body,
+        #viewDiv {
+          padding: 0;
+          margin: 0;
+          height: 100%;
+          width: 100%;
+        }
+      </style>
+
+      <link
+        rel="stylesheet"
+        href="https://js.arcgis.com/4.13/esri/themes/light/main.css"
+      />
+
+      <script>
+        require(["esri/views/MapView", "esri/WebMap"], function(MapView, WebMap) {
+          /************************************************************
+           * Creates a new WebMap instance. A WebMap must reference
+           * a PortalItem ID that represents a WebMap saved to
+           * arcgis.com or an on-premise portal.
+           *
+           * To load a WebMap from an on-premise portal, set the portal
+           * url with esriConfig.portalUrl.
+           ************************************************************/
+          var webmap = new WebMap({
+            portalItem: {
+              // autocasts as new PortalItem()
+              id: "c530e2143df04cbdaf8a70e93d3b3118"
+            }
+          });
+
+          /************************************************************
+           * Set the WebMap instance to the map property in a MapView.
+           ************************************************************/
+          var view = new MapView({
+            map: webmap,
+            container: "viewDiv"
+          });
+        });
+      </script>
+      `
+  )
+  console.log("LOADED 2")
+  }}    
+
+  triggerPageSwitch(element) {
+    // Parks with Native Title
+    // Edible Heat Map
+    // Vegetation Map 
+    // Basemap 
+    console
+    const index = parseInt(element['id']) - 1;
+    console.log(index);
+    console.log("here");
+    this.router.navigate('#/plant/describe');
+    
   }
 }
 
@@ -13,3 +95,4 @@ function scrollFunction() {
     console.log("not scrolling");
   }
 }
+
